56a9076b80758e0365a8986dfb033d230a9d3ef1
Kuruva Venkatesh <159419272+Venkatesh7981@users.noreply.github.com>
2026-08-12 16:19:13 +0530

    Time: 2 ms (99.30%) | Memory: 46.7 MB (34.19%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,34 +1,32 @@
 class Solution {
     public String reverseVowels(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        map.put('A',1);
-        map.put('E',1); 
-        map.put('I',1);
-        map.put('O',1);
-        map.put('U',1);
-        map.put('a',1);
-        map.put('e',1);
-        map.put('i',1);
-        map.put('o',1);
-        map.put('u',1);
-        char a[]=s.toCharArray();
-        int i=0;
-        int j=a.length-1;
-        while(i<j){
-            while(i<j && !map.containsKey(a[i])){
-                i++;
+        
+    char ans[]=s.toCharArray();
+    int i=0;
+    int j=ans.length-1;
+    while(i<j){
+        while(i<ans.length){
+            if(ans[i]=='a'|| ans[i]=='e' || ans[i]=='i' || ans[i]=='o' || ans[i]=='u'||
+            ans[i]=='A'|| ans[i]=='E' ||ans[i]=='U'|| ans[i]=='I' ||ans[i]=='O'){
+                break;
             }
-            while(i<j &&!map.containsKey(a[j])){
-                j--;
-            }
-            if(i<j){
-                char temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-                i++;
-                j--;
-            }
+            i++;
         }
-return new String(a);
+        while(j>=0){
+             if(ans[j]=='a'|| ans[j]=='e' || ans[j]=='i' || ans[j]=='o' || ans[j]=='u'||ans[j]=='A'|| ans[j]=='E' ||ans[j]=='U'|| ans[j]=='I' ||ans[j]=='O'){
+                break;
+            }
+            j--;
+        }
+        if(i<j){
+            char temp=ans[i];
+            ans[i]=ans[j];
+            ans[j]=temp;
+            i++;
+            j--;
+
+        }
+    }
+    return new String(ans);
     }
 }
