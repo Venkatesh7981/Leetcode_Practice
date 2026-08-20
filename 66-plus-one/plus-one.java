5cff4797bc6f529ea9cce4ca900eb2fdcf97f4ce
Kuruva Venkatesh <159419272+Venkatesh7981@users.noreply.github.com>
2026-08-20 17:16:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (43.38%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -2,14 +2,14 @@ class Solution {
     public int[] plusOne(int[] nums) {
 
       for(int i=nums.length-1;i>=0;i--){
-        if(nums[i]<9){
+        if(nums[i]<9){/// 8->9
             nums[i]++;
             return nums;
         }
-        nums[i]=0;
+        nums[i]=0;/// 59->60
       }
 
-  nums=new int[nums.length+1];
+  nums=new int[nums.length+1];////9999->10000
          nums[0]=1;
          return nums;
     }
