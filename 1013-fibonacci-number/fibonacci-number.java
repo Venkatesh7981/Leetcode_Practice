0374bfc411886bf6d00f89674af245322b44df1e
Kuruva Venkatesh <159419272+Venkatesh7981@users.noreply.github.com>
2026-09-03 18:00:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (85.69%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int a=0;
+        int b=1;
+        if(n<0) return n;
+        for(int i=1;i<=n;i++){
+           int c=a+b;
+             a=b;
+             b=c;
+        }
+        return a;
+    }
+}
